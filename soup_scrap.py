41fb4c51b994e7818422ec0a0a206ac3dfabb166
Thomas <thomas@Thomas-MacBook-Air.local>
2020-06-22 08:32:18 +0300

    working on scraping page 22.6.2020 08:30

--- soup_scrap.py
+++ soup_scrap.py
@@ -1,18 +1,66 @@
+"""
+
+--------Function Details-----------
+
+function recive Recive:  File Hand + page Url that it should scrap from
+
+function return : 1 for sucsess / -1 for fail
+
+----------Function Description-----------
+
+This function call each time with diffrent website Url and return 1 if succeded and -1 if not
+
+Its insett a data to the relevent page.
+
+? - Target of this page is to get the relevan part of the page.
+
+? - Title / Description / H1 / Keywords / Content / Images if there are.
+
+* - The content must be clean from any other non-releven objects
+
+* - Images should be inserted to a new folder that will contain all the website images
+
+
+"""
+
+
 from bs4 import BeautifulSoup
 import requests
 
-sample_url = "http://www.ask-tal.co.il/%D7%A7%D7%A0%D7%99%D7%99%D7%AA-%D7%93%D7%99%D7%A8%D7%94"
+
+def scrap_page(sample_url):
+    
+    page = requests.get(sample_url)
 
 
-page = requests.get(sample_url)
+    #print(page.text)
+
+    soup = BeautifulSoup(page.text, 'html.parser')
+
+    #print(soup.prettify())
+    print("--------script running----------------\n")
+   # print(soup.title)
+    #print(soup.h1)
+
+    keywords = soup.findAll("meta",  property="keywords")
+    #keywords =  soup.find(name="keywords")
+    #title = soup.find("meta",  property="og:title")
 
 
-#print(page.text)
 
-soup = BeautifulSoup(page.text, 'html.parser')
+    print(keywords)
 
-print(soup.prettify())
-print("------------------------\n")
-print(soup.title)
-print(soup.h1)
-print(soup.keywords)
+
+
+
+#Moudlue main for testing only
+if __name__ == '__main__':
+
+    
+
+    sample_url = "http://www.ask-tal.co.il/%D7%91%D7%9C%D7%A2%D7%93%D7%99%D7%95%D7%AA"
+    
+    
+    scrap_page(sample_url)
+
+
